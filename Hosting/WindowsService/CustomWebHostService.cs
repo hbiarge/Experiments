cda270cc4bdb59c5d061e7ae9165747b43281a50
Hugo Biarge <hugo@arosbi.com>
2018-06-23 21:11:49 +0200

    Asp.Net Core app as Windows Service

---
+++ Hosting/WindowsService/CustomWebHostService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.WindowsServices;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace WindowsService
+{
+    internal class CustomWebHostService : WebHostService
+    {
+        private readonly ILogger<CustomWebHostService> _logger;
+
+        public CustomWebHostService(IWebHost host) : base(host)
+        {
+            _logger = host.Services.GetRequiredService<ILogger<CustomWebHostService>>();
+        }
+
+        protected override void OnStarting(string[] args)
+        {
+            _logger.LogDebug("OnStarting method called.");
+            base.OnStarting(args);
+        }
+
+        protected override void OnStarted()
+        {
+            _logger.LogDebug("OnStarted method called.");
+            base.OnStarted();
+        }
+
+        protected override void OnStopping()
+        {
+            _logger.LogDebug("OnStopping method called.");
+            base.OnStopping();
+        }
+
+        protected override void OnStopped()
+        {
+            _logger.LogDebug("OnStopped method called.");
+            base.OnStopped();
+        }
+    }
+}
---
+++ Hosting/WindowsService/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Sinks.SystemConsole.Themes;
+
+namespace WindowsService
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console(
+                    outputTemplate: "[{Timestamp:HH:mm:ss zzz} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}",
+                    theme: AnsiConsoleTheme.Literate)
+                .CreateLogger();
+
+            var (isService, contentRoot) = GetExecutionMode(args);   
+
+            var webHostArgs = args.Where(arg => arg != "--console").ToArray();
+
+            var host = WebHost.CreateDefaultBuilder(webHostArgs)
+                .UseContentRoot(contentRoot)
+                .ConfigureLogging(builder =>
+                {
+                    builder.ClearProviders();
+                    builder.AddSerilog();
+                })
+                .UseStartup<Startup>()
+                .Build();
+
+            if (isService)
+            {
+                host.RunAsCustomService();
+            }
+            else
+            {
+                host.Run();
+            }
+        }
+
+        private static (bool isService, string contentRoot) GetExecutionMode(string[] args)
+        {
+            var notIsService = Debugger.IsAttached || args.Contains("--console");
+
+            if (notIsService)
+            {
+                return (false, Directory.GetCurrentDirectory());
+            }
+
+            var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
+            return (true, Path.GetDirectoryName(pathToExe));
+        }
+    }
+}
